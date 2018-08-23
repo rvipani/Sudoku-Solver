510c0b5e0f2d8f5a0e17dd6e7f11b68264080991
rvipani <rvipani@purdue.edu>
2018-08-22 21:22:07 -0700

    Started Testfile

---
+++ Testing.py
@@ -0,0 +1,12 @@
+import unittest
+import Main
+import GameReader
+
+class structureTest(unittest.TestCase):
+    def testGridSetup(self):
+        g = Main.Grid()
+        g.
+
+class importTest(unittest.TestCase):
+    def testimporting(self):
+        self.assertEqual(1,1)
